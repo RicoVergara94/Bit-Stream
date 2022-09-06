09c9a190ac5e6eb6af9518cb865c7aec155a48a6
Oscar Vergara <oscar.vergara1994@gmail.com>
2022-09-06 15:47:39 -0400

    added css to home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div id="login-container">
+        <input
+          id="input-email"
+          type="text"
+          placeholder="email or username"
+        ></input>
+        <input id="input-password" type="text" placeholder="password"></input>
+      </div>
+    </div>
+  );
 }
