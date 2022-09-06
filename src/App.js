fa471b077fc15d471dbd5dba4bef4fe24dd35cac
Oscar Vergara <oscar.vergara1994@gmail.com>
2022-09-06 17:02:54 -0400

    added routing functionality to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
+import Live from "./components/pages/Live/Live";
+import About from "./components/pages/About/About";
+import Contact from "./components/pages/Contact/Contact";
 
 export default function App() {
   return (
     <div>
-      <Home />
-      App
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/live" element={<Live />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
